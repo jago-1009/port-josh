7d119d4d945c2790bc00c9b08915d63641643221
Garwood, Jacob Leroy <jacogarw@iu.edu>
2025-02-19 13:35:57 -0500

    ``` fix: address code formatting issues and improve readability ```

--- src/index.js
+++ src/index.js
@@ -5,36 +5,8 @@
    * https://developers.google.com/explorer-help/code-samples#javascript
    */
   export const api_key = process.env.API_KEY;
-  export function test() {
+  export default function test() {
     console.log("test");
     console.log(api_key);
   }
-  export function authenticate() {
-    return gapi.auth2.getAuthInstance()
-        .signIn({scope: "https://www.googleapis.com/auth/youtube.readonly"})
-        .then(function() { console.log("Sign-in successful"); },
-              function(err) { console.error("Error signing in", err); });
-  }
-  export function loadClient() {
-    gapi.client.setApiKey(api_key);
-    return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
-        .then(function() { console.log("GAPI client loaded for API"); },
-              function(err) { console.error("Error loading GAPI client for API", err); });
-  }
-  // Make sure the client is loaded and sign-in is complete before calling this method.
-  export function execute() {
-    return gapi.client.youtube.playlistItems.list({
-      "part": [
-        "snippet,contentDetails"
-      ],
-      "playlistId": "PLADY51v0-uEBXrwk64v18nCdKZpUZnoIw"
-    })
-        .then(function(response) {
-                // Handle the results here (response.result has the parsed body).
-                console.log("Response", response);
-              },
-              function(err) { console.error("Execute error", err); });
-  }
-  gapi.load("client:auth2", function() {
-    gapi.auth2.init({client_id: "YOUR_CLIENT_ID"});
-  });
+ 
