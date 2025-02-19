3a4c4b8fb06afaae58b049893e80f561ee5ca734
Garwood, Jacob Leroy <jacogarw@iu.edu>
2025-02-19 13:37:14 -0500

    fix: update import in app.js to use test function from bundle.js and refactor exported functions

--- public/app.js
+++ public/app.js
@@ -1,23 +1,23 @@
-import * as bundle  from "./bundle.js";
+import test  from "./bundle.js";
 
 $(document).ready(function() {
     console.log("Document is ready!");
-    bundle.test();
+    test();
 });
-export function authenticate() {
+ function authenticate() {
     return gapi.auth2.getAuthInstance()
         .signIn({scope: "https://www.googleapis.com/auth/youtube.readonly"})
         .then(function() { console.log("Sign-in successful"); },
               function(err) { console.error("Error signing in", err); });
   }
-  export function loadClient() {
+   function loadClient() {
     gapi.client.setApiKey(api_key);
     return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
         .then(function() { console.log("GAPI client loaded for API"); },
               function(err) { console.error("Error loading GAPI client for API", err); });
   }
   // Make sure the client is loaded and sign-in is complete before calling this method.
-  export function execute() {
+   function execute() {
     return gapi.client.youtube.playlistItems.list({
       "part": [
         "snippet,contentDetails"
