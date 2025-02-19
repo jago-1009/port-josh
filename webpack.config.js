24b628ace4d3bd4fed0e4ebd5ad5967dbc4cf6c7
Garwood, Jacob Leroy <jacogarw@iu.edu>
2025-02-19 13:01:40 -0500

    Testing for webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 // webpack.config.js
-const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   // ... other configurations
   plugins: [
-    new Dotenv()
+    new EnvironmentPlugin({
+      NODE_ENV: 'development', // use 'development' unless process.env.NODE_ENV is defined
+      DEBUG: false,
+    }),
   ]
 };
