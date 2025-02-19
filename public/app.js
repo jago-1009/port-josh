d7e9a35462c88615795503a5c1824bebe3e61b09
Garwood, Jacob Leroy <jacogarw@iu.edu>
2025-02-19 13:12:44 -0500

    testing for obfustication.

--- public/app.js
+++ public/app.js
@@ -1,5 +1,7 @@
-import { authenticate, loadClient, execute } from "../src";
+let api_key = process.env.API_KEY;
 
 $(document).ready(function() {
+    console.log("ready!");
+    console.log(api_key)
     loadClient().then(authenticate).then(execute);
 });
