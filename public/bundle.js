0ceac4b94adec37ac113e59b641c74e91dca4e10
jago-1009 <125377105+jago-1009@users.noreply.github.com>
2025-02-22 17:25:48 -0500

    feat: add development build script and enhance video playlist functionality

---
+++ public/bundle.js
@@ -0,0 +1,1 @@
+var e={d:(o,r)=>{for(var t in r)e.o(r,t)&&!e.o(o,t)&&Object.defineProperty(o,t,{enumerable:!0,get:r[t]})},o:(e,o)=>Object.prototype.hasOwnProperty.call(e,o)},o={};e.d(o,{H:()=>r});const r=void 0;var t=o.H;export{t as api_key};
--- public/youtube.js
+++ public/youtube.js
@@ -2,7 +2,6 @@ import { api_key } from "./bundle.js";
 
 $(document).ready(function () {
   console.log("Document is ready!");
-  console.log(api_key);
   loadClient();
 });
 function loadClient() {
@@ -16,49 +15,78 @@ function loadClient() {
 }
 
 function execute() {
-  const playlistId = 'PLADY51v0-uEBXrwk64v18nCdKZpUZnoIw'; // Replace with your playlist ID
+  const playlistIdSolo = 'PLADY51v0-uEBXrwk64v18nCdKZpUZnoIw'; 
+  const playlistIdQuintet = 'PLADY51v0-uEATEtITAzTXRfwIsuU1UvVJ'; 
   gapi.client.youtube.playlistItems.list({
       part: "snippet,contentDetails",
-      playlistId: playlistId,
+      playlistId: playlistIdSolo,
       maxResults: 10
   }).then(function(response) {
       let videos = response.result.items;
       videos.forEach(video => {
         let title = "";
-        let artist = "";
-        let description = "";
+      
         // &#124; is the pipe character
         // &#58; is the colon character
    
 
         if (video.snippet.title.includes("|")) {
           title = `<h2>${video.snippet.title.split('|')[0].trim()}</h2`
-          console.debug(title)
 
-          if (title.includes(":")) {
-            artist = `<h3>By: ${title.split(":")[0].trim()}</h3>`
-            title = `<h2>${title.split(':')[1].trim()}</h2`
-            console.debug("ART", artist, "TITLE", title)
-          }
+          
         }
         else {
           title = video.snippet.title
         }
 
-        $("#pieces").append(` <div class="piece">
+        $("#pieces-solo").append(` <div class="piece">
             <iframe  src="https://www.youtube.com/embed/${video.contentDetails.videoId}?si=PCYc4DmLv_6xyYgJ"
                 title="YouTube video player" frameborder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                 referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
                 <div class="piece-info">
                     <h2>${title}</h2>
-                    ${artist}
-                    <p>${video.snippet.description}</p>
                 </div>
             </div>`)
       });
   }, function(err) {
       console.error("Error executing request", err);
   });
+  gapi.client.youtube.playlistItems.list({
+    part: "snippet,contentDetails",
+    playlistId: playlistIdQuintet,
+    maxResults: 10
+}).then(function(response) {
+    let videos = response.result.items;
+    videos.forEach(video => {
+      let title = "";
+    
+      // &#124; is the pipe character
+      // &#58; is the colon character
+ 
+
+      if (video.snippet.title.includes("|")) {
+        title = `<h2>${video.snippet.title.split('|')[0].trim()}</h2`
+
+        
+      }
+      else {
+        title = video.snippet.title
+      }
+
+      $("#pieces-quintet").append(` <div class="piece">
+          <iframe  src="https://www.youtube.com/embed/${video.contentDetails.videoId}?si=PCYc4DmLv_6xyYgJ"
+              title="YouTube video player" frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+              <div class="piece-info">
+                  <h2>${title}</h2>
+              </div>
+          </div>`)
+    });
+}, function(err) {
+    console.error("Error executing request", err);
+});
+  
 }
 
