854e2e72af75f4826179867b8b7455644d657180
Garwood, Jacob Leroy <jacogarw@iu.edu>
2025-02-26 15:19:52 -0500

    fix: update getRecords function to correctly handle API response and extract data fields

--- public/data.js
+++ public/data.js
@@ -3,27 +3,26 @@ import { airtable_key } from "./bundle.js";
 const airtableBase = 'appUqMYYmR2P2tnQK';
 
 const getRecords = async (base) => {
-	const response = await fetch(`https://api.airtable.com/v0/${airtableBase}/Details`, {
+	const request = await fetch(`https://api.airtable.com/v0/${airtableBase}/Details`, {
 		headers: {
 			Authorization: `Bearer ${airtable_key}`,
 		},
 	});
-	const data = await response.json();
-	// let fileURL = data[0].fields.data
-	// let aboutMeBlurb = data[1].fields.data;
-	console.log(data)
-	console.log(data[0])
-	console.log(data[0].fields)
+	const response = await request.json();
+	const data = response.records
+	let fileURL = data[0].fields.data
+	let aboutMeBlurb = data[1].fields.data;
 	
-	// let resume = document.getElementById('resume')
-	// if (resume) {
-	// 	return resume.setAttribute('src', fileURL)
-	// }
-	// let blurb = document.getElementById('blurb');
-	// if (blurb) {
-	// 	return blurb.innerHTML = aboutMeBlurb;
+	
+	let resume = document.getElementById('resume')
+	if (resume) {
+		return resume.setAttribute('src', fileURL)
+	}
+	let blurb = document.getElementById('blurb');
+	if (blurb) {
+		return blurb.innerHTML = aboutMeBlurb;
 
-	// }
+	}
 };
 
 getRecords();
