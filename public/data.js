957e421cb3a5d94daecd2cba28c42cba1e90dc5d
Garwood, Jacob Leroy <jacogarw@iu.edu>
2025-02-26 15:03:00 -0500

    fix: update getRecords function to remove base parameter and adjust API endpoint

--- public/data.js
+++ public/data.js
@@ -3,7 +3,7 @@ import { airtable_key } from "./bundle.js";
 const airtableBase = 'appUqMYYmR2P2tnQK';
 
 const getRecords = async (base) => {
-	const response = await fetch(`https://api.airtable.com/v0/${airtableBase}/${base}/Details`, {
+	const response = await fetch(`https://api.airtable.com/v0/${airtableBase}/Details`, {
 		headers: {
 			Authorization: `Bearer ${airtable_key}`,
 		},
@@ -13,4 +13,4 @@ const getRecords = async (base) => {
 	return data.records;
 };
 
-getRecords(airtable_key);
+getRecords();
